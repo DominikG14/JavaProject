96bb853d88665ca3f8bf7770b69896684a3b075e
DominikG14 <dgasiorek14@gmail.com>
2025-05-22 11:37:41 +0200

    Database init. Created User Entity

--- src/main/java/com/example/javaproject/JavaProjectApplication.java
+++ src/main/java/com/example/javaproject/JavaProjectApplication.java
@@ -2,8 +2,12 @@ package com.example.javaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.example.javaproject.entity")
+@EnableJpaRepositories("com.example.javaproject.repository")
 public class JavaProjectApplication {
 
     public static void main(String[] args) {

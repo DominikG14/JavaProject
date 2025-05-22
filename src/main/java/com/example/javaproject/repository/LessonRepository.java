6a5238bf1525baff7048b369a6548160e97d0635
DominikG14 <dgasiorek14@gmail.com>
2025-05-22 12:46:52 +0200

    Added repository classes for: User, Student, Tutor, Offer and Lesson

---
+++ src/main/java/com/example/javaproject/repository/LessonRepository.java
@@ -0,0 +1,10 @@
+package com.example.javaproject.repository;
+
+import com.example.javaproject.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+}
